f480219313f1dc7936d7c70e54e354ddf2f273ec
Max_ <mrom06@ya.ru>
2019-01-09 19:36:34 +0300

    Header full

--- src/components/header.js
+++ src/components/header.js
@@ -6,6 +6,7 @@ import "./../styles/header.css"
 export default class Header extends React.Component {
   render(){
     return <div id="header">
+      <img src="static/ENPROSPACE_LOGO_WHITE.png" height="50" width="88"/>
       <HeaderButton text="Главная" link="Main"/>
       <HeaderButton text="О нас" link="Main"/>
       <HeaderButton text="Услуги" link="Main"/>
