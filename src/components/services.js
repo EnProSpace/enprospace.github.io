063f59c2610825f772f1d9c4b52042276bb9aaf2
Max_ <mrom06@ya.ru>
2019-01-20 17:51:46 +0300

    Shop design

--- src/components/services.js
+++ src/components/services.js
@@ -6,15 +6,22 @@ import "./../styles/services.css"
 export default class Services extends React.Component {
   render() {
     let components = [];
-    for(let el in products){
-      components.push(<div id={el} className="product" key={el} onClick={()=>alert(el)}>
-        <img width="256" height="144" src={products[el].img}/>
-        {products[el].name}
-      
+    for (let el in products) {
+      components.push(<div id={el} className="product" key={el} onClick={() => alert(el)}>
+        <img width="256" height="144" src={products[el].img} />
+        <span style={{
+          fontSize: "25pt"
+        }}>{products[el].name}</span><br />
+        <span style={{
+          fontSize: "15pt"
+        }}>{products[el].microText}</span><br/><br/>
+        <button>
+          {products[el].buttonText}
+        </button>
       </div>)
     }
     return <div id="services">
-      {components}    
+      {components}
     </div >
   }
 }
