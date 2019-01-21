3bcadc34d77ec5ef8bb393a52e5e83164fa4f561
Max_ <mrom06@ya.ru>
2019-01-21 17:27:10 +0300

    Fixed babel runtime and non-visible text

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import { render } from "react-dom";
-import React from "react"
+import React from "react";
 
 import Header from "./components/header"
 import Body from "./components/body"
 
 import "./styles/main.css"
+import "@babel/polyfill";
 
 class MainComponent extends React.Component{
   render(){
